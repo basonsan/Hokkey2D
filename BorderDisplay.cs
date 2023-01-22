fa563041ffab615de4dde74f047bbd26440ffdb5
bason san <basonsan@yandex.ru>
2023-01-23 01:33:59 +0400

    ADD скрипт для движения камеры за шайбой
     с учетом размера спрайта

---
+++ BorderDisplay.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderDisplay : MonoBehaviour
+{
+    public static BorderDisplay Instance;
+    [SerializeField] private Vector2 screenResolution;
+    [SerializeField] private Camera cameraScene;
+    [SerializeField] private SpriteRenderer spriteStadion;
+
+    private Vector2 sizeCamera;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        if (Application.isEditor == false && Application.isPlaying == true)
+        {
+            screenResolution.x = Screen.width;
+            screenResolution.y = Screen.height;
+        }
+        //ширина и высота камеры
+        sizeCamera = RightTopPosition - LeftBottomPosition;
+    }
+
+    //левая позиция камеры
+    public Vector2 LeftBottomPosition
+    {
+        get
+        {
+            return cameraScene.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        }
+    }
+
+    //правая позиция камеры
+    public Vector2 RightTopPosition
+    {
+        get
+        {
+            return cameraScene.ScreenToWorldPoint(new Vector3(screenResolution.x, screenResolution.y, 0));
+        }
+    }
+
+    public Vector2 SizeCamera
+    {
+        get
+        {
+            return sizeCamera;
+        }
+    }
+
+    //правая координата спрайта
+    public Vector2 SizeSpriteBackground
+    {
+        get
+        {
+            return spriteStadion.size;
+        }
+    }
+}
---
+++ CameraMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] private PuckMove puck;
+    [SerializeField] private float speedMove;
+    [SerializeField] private Vector2 maxLeftTopBorder;
+    [SerializeField] private Vector2 maxRightBottomBorder;
+    [SerializeField] private Vector3 target;
+
+
+    private void Start()
+    {
+        target = puck.transform.position;
+    }
+    private void Update()
+    {
+        target = new Vector3(puck.transform.position.x, puck.transform.position.y, transform.position.z);
+
+        //двигаем камеру за шайбой, но если упираемся в край спрайта, камера останавливается
+        if (puck.transform.position.x - BorderDisplay.Instance.SizeCamera.x/2 < 0)
+            target = new Vector3(0 + BorderDisplay.Instance.SizeCamera.x / 2, target.y, transform.position.z);
+        if (puck.transform.position.x + BorderDisplay.Instance.SizeCamera.x / 2 > BorderDisplay.Instance.SizeSpriteBackground.x)
+            target = new Vector3(BorderDisplay.Instance.SizeSpriteBackground.x - BorderDisplay.Instance.SizeCamera.x/2, target.y, transform.position.z);
+        if (puck.transform.position.y - BorderDisplay.Instance.SizeCamera.y / 2 < 0)
+            target = new Vector3(target.x, 0 + BorderDisplay.Instance.SizeCamera.y / 2, transform.position.z);
+        if (puck.transform.position.y + BorderDisplay.Instance.SizeCamera.y / 2 > BorderDisplay.Instance.SizeSpriteBackground.y)
+            target = new Vector3(target.x, BorderDisplay.Instance.SizeSpriteBackground.y - BorderDisplay.Instance.SizeCamera.y / 2, transform.position.z);
+
+        //двигаем камеру
+        transform.position = Vector3.Lerp(transform.position, target, speedMove * Time.deltaTime);
+
+
+    }
+}
