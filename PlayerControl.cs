d4d83b6e9d6253092692868247c85ff83a04daa7
bason san <basonsan@yandex.ru>
2023-01-27 00:58:46 +0400

    IMP управление игроком
    Персонаж разворачивается. Можем брать шайбу, запускать его в нужном направлении... Есть баги с шайбой. Не хочет взаимодействовать с коллайдерами во время полета.

--- PlayerControl.cs
+++ PlayerControl.cs
@@ -1,34 +1,100 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody2D rb;
-    [SerializeField] float maxSpeed;
-    [SerializeField] float maxBoostSpeed;
-    [SerializeField] Transform centerStadion;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float maxBoostSpeed;
+    [SerializeField] private Transform puckPoint;
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private PuckMove puck;
+    [SerializeField] private float minStrongStrike;
+    [SerializeField] private float maxStrongStrike;
+    [SerializeField] private Gate gate;
+    [SerializeField] private BoxCollider2D bcTriger;
+    [SerializeField] private BoxCollider2D bcView;
     private Vector3 target;
     private float boostSpeed;
     private float speed;
-    private bool isTrigerOn;
+    private bool isTrigerBorderGateOn;
+    private float strongStrike;
+    private PlayerControl thisPlayer;
 
+    public event Action<float, Gate> StrikePuck;
+
+    public Transform PuckPoint => puckPoint;
     private void Awake()
     {
         speed = maxSpeed;
         boostSpeed = 1f;
-        isTrigerOn = false;
+        isTrigerBorderGateOn = false;
+        puck.OntrigerPlaer += CheckPlayer;
+        strongStrike = minStrongStrike;
+    }
+
+    private void CheckPlayer(PlayerControl player)
+    {
+        Debug.Log(player);
+        if (gameObject == player.gameObject)
+        {
+            Debug.Log("itsMe");
+        } else
+        {
+            Debug.Log("NoMe");
+        }
     }
 
     void Update()
     {
-        
+        Move();
+        if(Input.GetMouseButtonDown(0))
+        {
+            strongStrike += Time.deltaTime;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            StrikePuck.Invoke(strongStrike, gate);
+            strongStrike = minStrongStrike;
+            bcTriger.enabled = false;
+            bcView.enabled = false;
+        }
+    }
+
+    private void Move()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            if (bcTriger.enabled == false)
+            {
+                bcTriger.enabled = true;
+                bcView.enabled = true;
+            }
+        }
         target.x = transform.position.x + Input.GetAxis("Horizontal");
         target.y = transform.position.y + Input.GetAxis("Vertical");
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            if (!sprite.flipX)
+            {
+                sprite.flipX = true;
+                puckPoint.transform.localPosition *= -1;
+            }
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            if (sprite.flipX)
+            {
+                sprite.flipX = false;
+                puckPoint.transform.localPosition *= -1;
+            }
+        }
         target.z = transform.position.z;
         if (Input.GetAxis("Horizontal") == 0)
             boostSpeed = 1f;
-        if (isTrigerOn == false)
+        if (isTrigerBorderGateOn == false)
         {
             if (Input.GetAxis("Horizontal") == 1 && boostSpeed < maxBoostSpeed)
                 boostSpeed += Time.deltaTime;
@@ -36,31 +102,28 @@ public class PlayerControl : MonoBehaviour
                 boostSpeed += Time.deltaTime;
         }
         transform.position = Vector3.Lerp(transform.position, target, speed * boostSpeed * Time.deltaTime);
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        isTrigerOn = true;
-        /*while (isTrigerOn)
+        collision.transform.root.TryGetComponent<BorderStadion>(out BorderStadion borderStadion);
+        collision.transform.root.TryGetComponent<Gate>(out Gate gate);
+        if (borderStadion || gate)
         {
-            transform.position = Vector3.Lerp(transform.position, centerStadion.position, Time.deltaTime);
-        }*/
-        boostSpeed = 1f;
-        speed = 0.5f;
-        Debug.Log(collision);
+            isTrigerBorderGateOn = true;
+            boostSpeed = 1f;
+            speed = 0.5f;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        isTrigerOn = false;
-        Debug.Log("Triger off");
-        speed = maxSpeed;
-
-
+        collision.transform.root.TryGetComponent<BorderStadion>(out BorderStadion borderStadion);
+        collision.transform.root.TryGetComponent<Gate>(out Gate gate);
+        if (borderStadion || gate)
+        {
+            isTrigerBorderGateOn = false;
+            speed = maxSpeed;
+        }
     }
-
-
-
-
 }
---
+++ PuckMove.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuckMove : MonoBehaviour
+{
+    [SerializeField] float slowRate;
+    [SerializeField] BoxCollider2D bc2d;
+    private Transform player;
+    private bool isPlayerTakePuck;
+    private float speedPuck;
+    private Vector3 target;
+    private Vector3 velocity;
+    //private float speed;
+    public event Action<PlayerControl> OntrigerPlaer;
+    
+
+    private void Start()
+    {
+        isPlayerTakePuck = false;
+    }
+    private void Update()
+    {
+        if (isPlayerTakePuck)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position, 1f);
+        }
+        if (speedPuck > 0)
+        {
+            //transform.position = Vector3.MoveTowards(transform.position, target, speedPuck * Time.deltaTime);
+            //GetComponent<Rigidbody2D>().MovePosition(transform.position + target * Time.deltaTime);
+            transform.position += velocity * Time.deltaTime;
+            speedPuck -= Time.deltaTime * slowRate;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.root.TryGetComponent<PlayerControl>(out PlayerControl playerControl);
+        if (playerControl)
+        {
+            isPlayerTakePuck = true;
+            player = playerControl.PuckPoint;
+            OntrigerPlaer.Invoke(playerControl);
+            playerControl.StrikePuck += SetParametrPuck;
+            bc2d.enabled = false;
+        }
+        collision.transform.root.TryGetComponent<BorderStadion>(out BorderStadion borderStadion);
+        if (borderStadion)
+        {
+            velocity *= -1;
+        }
+        Debug.Log(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.transform.root.TryGetComponent<PlayerControl>(out PlayerControl playerControl);
+        if (playerControl)
+        {
+            isPlayerTakePuck = false;
+            playerControl.StrikePuck -= SetParametrPuck;
+            bc2d.enabled = true;
+
+        }
+    }
+
+    private void SetParametrPuck(float speed, Gate gate)
+    {
+        speedPuck = speed;
+        target = gate.transform.position;
+        velocity = target - transform.position;
+    }
+
+}
